bcd3df75de3b2a62f9a9ec15bc6b4c556e9cc97c
itowaka044 <joao.waka@gmail.com>
2025-07-03 00:47:10 -0300

    estudos 01/07 - 03/07, conteudos estudados: .env e docker-compose, estrutura do projeto, orm eloquent, entendendo o mvc do laravel, conceito de fillable para muitas props

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Http\Response;
+
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+
+
+    // testando view()
+
+    public function index(){
+
+        return view('create');
+
+    }
+
+
+
+    // [] read all products
+
+    public function readAllProducts(){
+
+        $products = Product::all();
+
+        return $products;
+
+    }
+
+    // [ok] create products 
+
+    public function createProduct(Request $req){
+        
+        $prodReq = $req->validate(
+        [ 
+            'name' => 'required|string',
+            'price' => 'required|integer',
+            'quantity' => 'required|integer',
+        ],
+        [
+            'name.required' => 'campo "name" obrigatório',
+            'price.required' => 'campo "price" obrigatório',
+            'quantity.required' => 'campo "quantity" obrigatório',
+        ]);
+
+        $product = Product::create($prodReq);
+
+        return response()->json($product, 201);
+
+    }
+
+    // [] read product by id
+
+
+    // [] update product
+
+
+    // [] update product price only
+
+
+    // [] delete product
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProductController;
+
+// [ok] read all products
+
+Route::get("/products", [ProductController::class, 'readAllProducts']);
+
+// [ok] create products 
+
+Route::post("/products", [ProductController::class, 'createProduct']);
+
+
+// [] read product by id
+
+
+// [] update product
+
+
+// [] update product price only
+
+
+// [] delete product
+
+Route::get('/create', [ProductController::class, 'index']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
